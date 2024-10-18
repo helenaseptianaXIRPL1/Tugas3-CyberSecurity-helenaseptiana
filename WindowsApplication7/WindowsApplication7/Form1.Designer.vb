c1a0c72d2f4d41ca40047cfca60052d464ad1131
HelenaS <88118215+helenaseptianaXIRPL1@users.noreply.github.com>
2024-10-18 15:09:02 +0700

    Add files via upload

---
+++ WindowsApplication7/WindowsApplication7/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Notepad
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFolderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(154, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(541, 429)
+        Me.TextBox1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(695, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.SaveToolStripMenuItem, Me.ExitToolStripMenuItem, Me.OpenFolderToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NewToolStripMenuItem.Text = "new"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OpenToolStripMenuItem.Text = "open"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveToolStripMenuItem.Text = "save"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "exit"
+        '
+        'OpenFolderToolStripMenuItem
+        '
+        Me.OpenFolderToolStripMenuItem.Name = "OpenFolderToolStripMenuItem"
+        Me.OpenFolderToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.OpenFolderToolStripMenuItem.Text = "open folder"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(10, 31)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(144, 407)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Notepad
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(695, 444)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Notepad"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFolderToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ WindowsApplication7/WindowsApplication7/Form1.vb
@@ -0,0 +1,87 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Notepad
+    Private Sub Notepad_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Me.Text = "Notepad"
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles NewToolStripMenuItem.Click
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Using openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "All Files (*.*)|*.*"
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                Try
+                    Dim fileContent As String = System.IO.File.ReadAllText(openFileDialog.FileName)
+                    TextBox1.Text = fileContent ' Menampilkan isi file
+                Catch ex As Exception
+                    MessageBox.Show("Error reading file: " & ex.Message, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            End If
+        End Using
+    End Sub
+
+    Private Sub OpenFolderToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles OpenFolderToolStripMenuItem.Click
+        Using folderBrowserDialog As New FolderBrowserDialog()
+            If folderBrowserDialog.ShowDialog() = DialogResult.OK Then
+                Dim files As String() = Directory.GetFiles(folderBrowserDialog.SelectedPath)
+                ListBox1.Items.Clear() ' Mengosongkan ListBox sebelumnya
+
+                For Each file In files
+                    ListBox1.Items.Add(Path.GetFileName(file)) ' Menambahkan nama file ke ListBox
+                Next
+
+                If ListBox1.Items.Count > 0 Then
+                    MessageBox.Show("Files found in the selected folder. Select one to open.", "Files Loaded", MessageBoxButtons.OK)
+                Else
+                    MessageBox.Show("No files found in the selected folder.", "No Files", MessageBoxButtons.OK)
+                End If
+            End If
+        End Using
+    End Sub
+
+    Private Sub ListBox1_DoubleClick(ByVal sender As Object, ByVal e As EventArgs) Handles ListBox1.DoubleClick
+        If ListBox1.SelectedItem IsNot Nothing Then
+            Dim selectedFile As String = ListBox1.SelectedItem.ToString()
+            Dim folderPath As String = Path.GetDirectoryName(ListBox1.Items(0).ToString())
+            Dim filePath As String = Path.Combine(folderPath, selectedFile)
+
+            Try
+                Dim fileContent As String = System.IO.File.ReadAllText(filePath)
+                TextBox1.Text = fileContent ' Menampilkan isi file
+            Catch ex As Exception
+                MessageBox.Show("Unable to display content of the file: " & ex.Message, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Using saveFileDialog As New SaveFileDialog()
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*"
+            If saveFileDialog.ShowDialog() = DialogResult.OK Then
+                Dim encryptedText As String = Encrypt(TextBox1.Text)
+                System.IO.File.WriteAllText(saveFileDialog.FileName, encryptedText)
+            End If
+        End Using
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    ' Enkripsi menggunakan nilai ASCII
+    Private Function Encrypt(ByVal input As String) As String
+        Dim key As Integer = 3 ' Kunci pergeseran
+        Dim output As New StringBuilder()
+
+        For Each c As Char In input
+            ' Menggeser nilai ASCII setiap karakter
+            output.Append(Chr(Asc(c) + key))
+        Next
+
+        Return output.ToString()
+    End Function
+End Class
